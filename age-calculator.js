108ca182029a243c9fcd7945a2834f5a93529d9a
adamclement <adamadc@gmail.com>
2022-02-04 00:07:46 +0000

    Use function to calculate age

---
+++ age-calculator.js
@@ -0,0 +1,8 @@
+// Functions - Age Calculator
+
+const ageCalculator = function (name, yearOfBirth, currentYear) {
+  return `${name} is ${currentYear - yearOfBirth} years old.`;
+};
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
