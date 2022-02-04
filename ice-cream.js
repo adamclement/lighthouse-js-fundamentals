033195a4387f579e51d7e381f56cb62042efaa3d
adamclement <adamadc@gmail.com>
2022-02-04 04:31:03 +0000

    Manipulating arrays

---
+++ ice-cream.js
@@ -0,0 +1,19 @@
+// Arrays
+
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+console.log(iceCreamFlavours);
+
+// Use push to add element to end of Array
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// Get first element of array
+console.log(iceCreamFlavours[0]);
+
+// Get last element of the array
+console.log(iceCreamFlavours[(iceCreamFlavours.length - 1)]);
+
+// Get total number of elements in array
+console.log(iceCreamFlavours.length);
+
